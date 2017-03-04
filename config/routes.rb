3e39d061ededa0d40527f5dc6b7a63d82a62e13a
leyyaps <andrew.smith@ipsofactouk.com>
2017-03-04 12:45:07 +0000

    installed active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  resources :posts, only: [:show, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'static_pages#home'
